c9ce5d76da46ae8d4cc67c677e3822e418aa942b
yuris <yls4@cornell.edu>
2023-10-19 10:25:31 -0400

    added rest to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Example from "./scripts/example";
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("hello from index.js");
-    let ex = new Example()
-    
+    let div = document.querySelector("div");
+    let ex = new Example(div);
+
 })
