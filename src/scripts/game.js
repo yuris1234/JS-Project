98664286c7ebeccdcdb4cf30f2d60e3f843c27de
yuris <yls4@cornell.edu>
2023-10-23 11:50:11 -0400

    got note audio working, now working on collision with bucket

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,11 +2,12 @@ import Bucket from "./bucket";
 import Note from "./note";
 
 class Game {
-    constructor(canvas, ctx) {
+    constructor(canvas, ctx, audioCtx) {
         // debugger
         this.bucket = new Bucket(canvas.width, canvas.height, ctx);
         this.notes = [];
         this.ctx = ctx;
+        this.audioCtx = audioCtx;
         this.canvas = canvas;
         this.newNotes();
         this.animate();
@@ -19,34 +20,46 @@ class Game {
     newNotes() {
         for (let i = 0; i < 10; i++) {
             // debugger
-            let newNote = new Note(this.canvas, this.ctx);
+            let newNote = new Note(this.canvas, this.ctx, this.audioCtx);
             this.notes.push(newNote);
         }
         // debugger
     }
 
     animate() {
-        // this.ctx.fillStyle = 'white';
-        // this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.bucket.update();
+
         let notesHolder = [];
         this.notes.forEach((note) => {
             if (note.appear) {
                 notesHolder.push(note);
             }
+            note.checkCollision(this.bucket);
             note.update();
         })
         this.notes = notesHolder;
         while (this.notes.length < 10) {
-            let newNote = new Note(this.canvas, this.ctx);
+            let newNote = new Note(this.canvas, this.ctx, this.audioCtx);
             this.notes.push(newNote);
         } 
-        // this.bucket.update();
-        // this.ctx.fillStyle = 'white';
-        // this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     
         let boundAnimate = this.animate.bind(this);
         window.requestAnimationFrame(boundAnimate);
     }
+
+    // createNoteTable() {
+    //     const noteFreq = [];
+    //     for (let i = 4; i < 9; i++) {
+    //         noteFreq[i] = [];
+    //     }
+    //     noteFreq[4]['C'] = 261.6;
+    //     noteFreq[4]['D'] = 277.2;
+    //     noteFreq[4]['E'] = 329.6;
+    //     noteFreq[4]['F'] = 349.2;
+    //     noteFreq[4]['G'] = 392.0;
+    //     noteFreq[4]['A'] = 440.0;
+    //     noteFreq[4]['B'] = 493.9;
+    //     noteFreq[5]['C'] = 523.3;
+    // }
 }
 export default Game;
--- src/scripts/note.js
+++ src/scripts/note.js
@@ -1,13 +1,19 @@
+import noteFreq from "./noteTable";
+
 class Note {
-    constructor(canvas, ctx) {
+    constructor(canvas, ctx, audioCtx) {
+
         this.velocity = this.getRandomArbitrary(1, 3);
         this.pos = {
             x: this.getRandomArbitrary(100, canvas.width),
             y: 0
         };
-        this.appear = true;
+        this.audioCtx = audioCtx
         this.ctx = ctx;
         this.canvas = canvas;
+        this.appear = true;
+        this.collision = false;
+        this.freq = noteFreq[this.getRandomInt(0,1)][this.getRandomAlpha()];
     }
 
     draw() {
@@ -21,6 +27,7 @@ class Note {
     update() {
         if (this.pos.y >= this.canvas.height- 100) {
             this.appear = false;
+            this.playTone();
         } else {
             this.draw();
             this.pos.y += this.velocity;
@@ -31,11 +38,40 @@ class Note {
         return Math.random() * (max - min) + min;
     }
 
-    // getRandomInt(min, max) {
-    //     min = Math.ceil(min);
-    //     max = Math.floor(max);
-    //     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-    //   }
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+    }
+
+    getRandomAlpha() {
+        let alpha = ['A','B','C','D','E','F','G'];
+        let num = this.getRandomInt(0,6);
+        return alpha[num];
+    }
+
+    playTone() {
+        // debugger
+        const osc = this.audioCtx.createOscillator();
+        osc.type = "sawtooth";
+        osc.frequency.value = this.freq;
+        // (this.freq, this.audioCtx.currentTime)
+        osc.connect(this.audioCtx.destination)
+        osc.start();
+        osc.stop(this.audioCtx.currentTime + 0.1);
+    }
+
+    // checkCollision(bucket) {
+    //     if (
+    //         this.pos.x <= bucket.pos + 100 &&
+    //         this.pos.y >= this.canvas.height - 100 &&
+    //         this.pos.x >= bucket.pos - 100 &&
+    //         this.pos.y <= this.canvas.height + 100 
+    //     ) {
+    //         this.collision = true;
+
+    //     }
+    // }
 
 }
 export default Note;
